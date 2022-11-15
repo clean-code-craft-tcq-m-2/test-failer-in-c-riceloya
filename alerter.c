a71dcd250cf8bcbf0183ecab8a24865c6f0088df
riceloya <109813509+riceloya@users.noreply.github.com>
2022-11-15 15:15:52 -0600

    alerter error pointer

--- alerter.c
+++ alerter.c
@@ -21,6 +21,7 @@ void alertInCelcius(float farenheit) {
         // Add a test below to catch this bug. Alter the stub above, if needed.
         alertFailureCount += 0;
     }
+    assert(celcius<returnCode);
 }
 
 int main() {
