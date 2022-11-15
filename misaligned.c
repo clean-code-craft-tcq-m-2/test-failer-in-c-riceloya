2e56187a6eb4045cc3aa687b7ed5e97c589c6dae
riceloya <109813509+riceloya@users.noreply.github.com>
2022-11-15 13:11:30 -0600

    cable test 00

--- misaligned.c
+++ misaligned.c
@@ -4,12 +4,14 @@
 int printColorMap() {
     const char* majorColor[] = {"White", "Red", "Black", "Yellow", "Violet"};
     const char* minorColor[] = {"Blue", "Orange", "Green", "Brown", "Slate"};
-    int i = 0, j = 0;
+    int i = 0, j = 0, k = 0;
     for(i = 0; i < 5; i++) {
         for(j = 0; j < 5; j++) {
-            printf("%d | %s | %s\n", i * 5 + j, majorColor[i], minorColor[i]);
+            k = i*5+j;
+            printf("%d | %s | %s\n", k, majorColor[i], minorColor[i]);
         }
     }
+    assert(k==(i*j));
     return i * j;
 }
 
